1e2f87344d4923a99badf82461255ce1a9b72ab1
mikeygbd <mikey.soares@gmail.com>
2019-02-05 22:34:09 -0500

    commented out mount snow because it was breaking the program.

--- lib/snow_report/report.rb
+++ lib/snow_report/report.rb
@@ -9,7 +9,7 @@ attr_accessor :name, :temp, :lifts, :trails, :new_snow, :parks, :status, :url
 
   def self.scrape_reports
     reports = []
-    reports << self.scrape_mountsnow
+    # reports << self.scrape_mountsnow
     reports << self.scrape_stratton
     reports << self.scrape_hunter
     reports << self.scrape_mountaincreek
@@ -17,20 +17,20 @@ attr_accessor :name, :temp, :lifts, :trails, :new_snow, :parks, :status, :url
     reports << self.scrape_stowe
     reports
   end
-  #
-  def self.scrape_mountsnow
-    doc = Nokogiri::HTML(open("https://www.mountsnow.com/ski-ride/snow-report/"))
-    doc2 = Nokogiri::HTML(open("https://www.onthesnow.com/vermont/mount-snow/ski-resort.html"))
-    report = self.new
-    report.name = doc2.search(".resort_name").text
-    report.status = doc2.search(".current_status").text
-    report.trails = doc.search(".value").first.text
-    report.lifts = doc.search(".value")[1].text
-    report.temp = doc.search(".value")[9].text
-    report.new_snow = doc.search(".value")[4].text
-    report.parks = doc.search(".value")[17].text
-    # binding.pry
-  end
+
+  # def self.scrape_mountsnow
+  #   # doc = Nokogiri::HTML(open("https://www.mountsnow.com/ski-ride/snow-report/"))
+  #   doc2 = Nokogiri::HTML(open("https://www.onthesnow.com/vermont/mount-snow/ski-resort.html"))
+  #   report = self.new
+  #   report.name = doc2.search(".resort_name").text
+  #   report.status = doc2.search(".current_status").text
+  #   # report.trails = doc.search(".value").first.text
+  #   # report.lifts = doc.search(".value")[1].text
+  #   # report.temp = doc.search(".value")[9].text
+  #   # report.new_snow = doc.search(".value")[4].text
+  #   # report.parks = doc.search(".value")[17].text
+  #   # binding.pry
+  # end
 
   def self.scrape_stratton
     doc = Nokogiri::HTML(open("https://www.onthesnow.com/vermont/stratton-mountain/skireport.html"))
