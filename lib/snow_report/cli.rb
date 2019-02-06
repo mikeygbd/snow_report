6fb0909355e7b23db4482ffe3c1aa36de306b6ba
mikeygbd <mikey.soares@gmail.com>
2019-02-05 23:15:23 -0500

    started creating the layout of the report in cli.rb

--- lib/snow_report/cli.rb
+++ lib/snow_report/cli.rb
@@ -1,6 +1,6 @@
 #CLI Controller
 class SnowReport::CLI
-  attr_accessor  :name, :temp, :trails
+
 
   def call
     list_reports
@@ -12,7 +12,7 @@ class SnowReport::CLI
     puts "Today's Snow Reports:"
     @reports = SnowReport::Report.today
     @reports.each_with_index do |report, i|
-      puts "#{i + 1}. #{report.name} - #{report.temp} - #{report.trails}"
+      puts "#{i + 1}. #{report.name}"
     end
   end
 
@@ -23,7 +23,20 @@ class SnowReport::CLI
       input = gets.strip
       if input.to_i > 0
         the_report = @reports[input.to_i-1]
-        puts "#{the_report.name} - #{the_report.temp} - #{the_report.trails}"
+        puts "-----------#{the_report.name}------------"
+        puts ""
+        puts "Status: #{the_report.status}"
+        puts "Summit Tempature: #{the_report.temp}"
+        puts "Trails Open: #{the_report.trails}"
+        puts "Lifts Open: #{the_report.lifts}"
+        puts "New Snow: #{the_report.new_snow}"
+        puts "Parks Open: #{the_report.parks}"
+        puts "Website: #{the_report.url}"
+        puts ""
+        # puts -----------#description------------
+        # puts ""
+        # puts "#{the_report.description}"
+        # puts ""
       elsif input == "list"
         list_reports
       else
